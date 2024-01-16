c0ff62a59a9ccb6e4bfb763ab7c4d8a16724feac
L-Pircy <taveesha.agarwal@gmail.com>
2024-01-16 22:06:50 +0530

    basic types, arrays and union

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,32 @@
 "use strict";
+// basic Types
 let id = 5;
-console.log("ID: ", id);
+let company = "Taveesha";
+let isPublished = true;
+let x = "hi";
+let age = 3;
+// arrays
+let ids = [1, 2, 3, 4];
+let arr = [1, true, "text"];
+// tuple
+let person = [1, "text", false];
+// tuple array, ie array of tuples
+let employee;
+employee = [
+    [1, "esha"],
+    [2, "pircy"],
+    [3, "lil"],
+];
+// union, be more than 1 type
+let pid;
+pid = [1, 2];
+pid = "22";
+pid = false;
+// enum, sset of named constants (?)
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,35 @@
+// basic Types
 let id: number = 5;
-console.log("ID: ", id);
+let company: string = "Taveesha";
+let isPublished: boolean = true;
+let x: any = "hi";
+let age: number = 3;
+
+// arrays
+let ids: number[] = [1, 2, 3, 4];
+let arr: any[] = [1, true, "text"];
+
+// tuple
+let person: [number, string, boolean] = [1, "text", false];
+
+// tuple array, ie array of tuples
+let employee: [number, string][];
+employee = [
+  [1, "esha"],
+  [2, "pircy"],
+  [3, "lil"],
+];
+
+// union, be more than 1 type
+let pid: string | number | boolean | number[];
+pid = [1, 2];
+pid = "22";
+pid = false;
+
+// enum, sset of named constants (?)
+enum Direction1 {
+  Up,
+  Down,
+  Left,
+  Right,
+}
